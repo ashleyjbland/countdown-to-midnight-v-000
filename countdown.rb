599d593838733f4f0c45fd6dd1adb84a81fa7073
Ashley Bland <ashleyjbland@gmail.com>
2018-08-29 22:38:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,10 @@
 def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
+    number -= 1
+    if number == 0
+    return "HAPPY NEW YEAR!"
+  end
   end
 end
-puts "HAPPY NEW YEAR!"
+
